faf89cf8c213d7ddc27a550668ea45c46335cbae
MihaVar <mykhailvarych@gmail.com>
2023-09-15 20:36:42 +0300

    book and lib classes

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Book {
+    private String Name;
+
+    private String Author;
+
+    private long ISBN;
+
+    private int Year;
+
+    public Book(String name, String author, long ISBN, int year) {
+        this.Name = name;
+        this.Author = author;
+        this.ISBN = ISBN;
+        this.Year = year;
+    }
+
+}
